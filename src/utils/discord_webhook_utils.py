627589946a00e7c096bf6a20118dd88a7eb519e6
silentesc <scope-frisk-copy@duck.com>
2025-08-17 18:56:03 +0200

    Discord Webhook check for 429 and wait for retry_after seconds

--- src/utils/discord_webhook_utils.py
+++ src/utils/discord_webhook_utils.py
@@ -1,3 +1,4 @@
+import time
 import typing
 import requests
 from enum import Enum
@@ -21,14 +22,20 @@ class DiscordWebhookUtils:
         if not self.webhook_url:
             return
 
-        response = requests.post(self.webhook_url, json=json)
-        try:
-            response.raise_for_status()
-        except requests.HTTPError as e:
-            logger.error(json)
-            logger.error(f"Error while sending discord webhook embed: {response.status_code} {response.text}")
-            logger.error(e)
-        logger.trace(f"Request to discord returned status code {response.status_code}")
+        while True:
+            response = requests.post(self.webhook_url, json=json)
+            if response.status_code == 204:
+                logger.trace("Discord webhook sent successfully")
+                return
+            elif response.status_code == 429:
+                data = response.json()
+                retry_after = data.get("retry_after", 1000)
+                logger.warning(f"Discord webhook rate limited. Retrying after {round(retry_after, 2)}s")
+                time.sleep(retry_after)
+                continue
+            else:
+                logger.error(f"Failed to send discord webhook: {response.status_code} - {response.text}")
+                return
 
 
     def send_webhook_embed(self, webhook_type: DiscordWebhookType, title: str, description: str = "", content: str = "", fields: list[dict[str, str | bool]] = []) -> None:
