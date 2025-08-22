cf850268fa5b66205182cd75d2e87671394885f6
silentesc <scope-frisk-copy@duck.com>
2025-08-22 21:23:19 +0200

    fix bug with strike_type value

--- src/utils/strike_utils.py
+++ src/utils/strike_utils.py
@@ -53,7 +53,7 @@ class StrikeUtils:
 
     def cleanup_db(self, hashes: list[str]) -> None:
         with DbManager() as db:
-            rows = db.execute_fetchall(query=f"SELECT hash FROM {self.strike_type}_strikes GROUP BY hash")
+            rows = db.execute_fetchall(query=f"SELECT hash FROM {self.strike_type.value}_strikes GROUP BY hash")
 
         for row in rows:
             torrent_hash = row["hash"]
@@ -78,7 +78,7 @@ class StrikeUtils:
 
     def get_consecutive_days(self) -> int:
         with DbManager() as db:
-            rows = db.execute_fetchall(query=f"SELECT * FROM {self.strike_type}_strikes WHERE hash = ? ORDER BY timestamp DESC", params=(self.torrent_hash,))
+            rows = db.execute_fetchall(query=f"SELECT * FROM {self.strike_type.value}_strikes WHERE hash = ? ORDER BY timestamp DESC", params=(self.torrent_hash,))
 
         if not rows:
             return 0
